fb689da6110daa1f62d5150158bf1e4634bfee9f
Isak W <isak.wertwein@gmail.com>
2018-06-22 00:59:53 +0200

    Make SmartBytesFormatter make use of other components

--- src/main/java/org/aesy/bytes/format/SmartBytesFormatter.java
+++ src/main/java/org/aesy/bytes/format/SmartBytesFormatter.java
@@ -1,22 +1,17 @@
 package org.aesy.bytes.format;
 
-import org.aesy.bytes.ByteUnit;
 import org.aesy.bytes.Bytes;
+import org.aesy.bytes.convert.BytesConverter;
+import org.aesy.bytes.convert.NaturalBytesConverter;
 
-import java.math.BigDecimal;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.util.*;
+import java.util.Locale;
+import java.util.Objects;
 
 public class SmartBytesFormatter implements BytesFormatter {
-    private static final List<ByteUnit> SI = Arrays.asList(ByteUnit.SI.values());
-    private static final List<ByteUnit> IEC = Arrays.asList(ByteUnit.IEC.values());
-    private static final List<ByteUnit> JEDEC = Arrays.asList(ByteUnit.JEDEC.values());
-    private static final BigDecimal ONE_THOUSAND = BigDecimal.valueOf(1000);
     private static final int DEFAULT_PRECISION = 2;
+    private static final BytesConverter humanReadableConverter = new NaturalBytesConverter();
 
-    private Locale locale;
-    private int precision;
+    private final BytesFormatter formatter;
 
     public SmartBytesFormatter() {
         this(Locale.getDefault(Locale.Category.FORMAT), DEFAULT_PRECISION);
@@ -33,127 +28,13 @@ public class SmartBytesFormatter implements BytesFormatter {
     public SmartBytesFormatter(Locale locale, int precision) {
         Objects.requireNonNull(locale);
 
-        this.locale = locale;
-        this.precision = precision;
+        this.formatter = new SimpleBytesFormatter(locale, precision);
     }
 
     @Override
-    public String toShortString(Bytes bytes) {
+    public String format(Bytes bytes) {
         Objects.requireNonNull(bytes);
 
-        BigDecimal value = bytes.getValue();
-        int scale = value.stripTrailingZeros().scale();
-
-        NumberFormat formatter = DecimalFormat.getNumberInstance(locale);
-        formatter.setMinimumFractionDigits(Math.max(0, Math.min(precision, scale)));
-        formatter.setMaximumFractionDigits(precision);
-
-        String formattedValue = formatter.format(value);
-        String unitAbbreviation = bytes.getUnit().getAbbreviation();
-
-        return String.format("%s %s", formattedValue, unitAbbreviation);
-    }
-
-    @Override
-    public String toLongString(Bytes bytes) {
-        Objects.requireNonNull(bytes);
-
-        BigDecimal value = bytes.getValue();
-        int scale = value.stripTrailingZeros().scale();
-
-        NumberFormat formatter = DecimalFormat.getNumberInstance(locale);
-        formatter.setMinimumFractionDigits(Math.max(0, Math.min(precision, scale)));
-        formatter.setMaximumFractionDigits(precision);
-
-        String formattedValue = formatter.format(value);
-        String unitName = bytes.getUnit().getName();
-
-        return String.format("%s %s", formattedValue, unitName);
-    }
-
-    @Override
-    public String toHumanReadableShortString(Bytes bytes) {
-        Objects.requireNonNull(bytes);
-
-        return toShortString(convertToHumanReadableBytes(bytes));
-    }
-
-    @Override
-    public String toHumanReadableLongString(Bytes bytes) {
-        Objects.requireNonNull(bytes);
-
-        return toLongString(convertToHumanReadableBytes(bytes));
-    }
-
-    private Bytes convertToHumanReadableBytes(Bytes bytes) {
-        Bytes originalBytes = bytes;
-        BigDecimal originalValue = originalBytes.getValue();
-        ByteUnit originalUnit = originalBytes.getUnit();
-        boolean isLessThanOneThousandBytes = ByteUnit.BYTE.convert(originalBytes)
-                                                          .getValue().compareTo(ONE_THOUSAND) < 0;
-
-        if (isLessThanOneThousandBytes) {
-            // Values less than 1000 bytes are best viewed as bytes
-            return originalBytes;
-        }
-
-        ArrayList<ByteUnit> units = new ArrayList<>();
-
-        if (SI.contains(originalUnit)) {
-            // Prefer SI units
-            units.addAll(SI);
-            units.addAll(IEC);
-        } else if (IEC.contains(originalUnit)) {
-            // Prefer IEC units
-            units.addAll(IEC);
-            units.addAll(SI);
-        } else if (JEDEC.contains(originalUnit)) {
-            // Only use JEDEC units if original does to reduce ambiguity
-            units.addAll(JEDEC);
-        } else {
-            // Unknown unit type, just use original
-            return originalBytes;
-        }
-
-        Bytes best = originalBytes;
-
-        for (ByteUnit unit : units) {
-            Bytes newBytes = unit.convert(originalBytes);
-            BigDecimal value1 = newBytes.getValue().stripTrailingZeros();
-            BigDecimal value2 = best.getValue().stripTrailingZeros();
-
-            boolean isLessThanOne = value1.compareTo(BigDecimal.ONE) < 0;
-
-            if (isLessThanOne) {
-                // Values less than 1.0 not determined readable, skip
-                continue;
-            }
-
-            boolean isBetterValue = value1.compareTo(value2) < 0;
-            boolean hasBetterScale = value1.scale() < value2.scale();
-            boolean isLessThanOneThousand = best.getValue().compareTo(ONE_THOUSAND) < 0;
-
-            if (isBetterValue || (isLessThanOneThousand && hasBetterScale)) {
-                best = newBytes;
-            }
-        }
-
-        return best;
-    }
-
-    public static void main(String[] args) {
-        Locale.setDefault(Locale.Category.FORMAT, Locale.US);
-        BytesFormatter formatter = new SmartBytesFormatter();
-
-        Bytes c1 = ByteUnit.SI.MEGABYTE.convert(Bytes.valueOf(1024, ByteUnit.IEC.KIBIBYTE));
-        Bytes c2 = ByteUnit.IEC.MEBIBYTE.convert(Bytes.valueOf(1024, ByteUnit.IEC.KIBIBYTE));
-
-        System.out.println(c1.getValue().scale());
-        System.out.println(c2.getValue().scale());
-
-        System.out.println(formatter.toHumanReadableShortString(c1));
-        System.out.println(formatter.toHumanReadableShortString(c2));
-
-        System.out.println(formatter.toShortString(Bytes.valueOf(new BigDecimal("1.00"), ByteUnit.BYTE)));
+        return formatter.format(humanReadableConverter.convert(bytes));
     }
 }
